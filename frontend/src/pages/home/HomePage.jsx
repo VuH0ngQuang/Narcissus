a4398e9f3b22fdb91125fb6d8e67b705562b034b
VuH0ngQuang <vuhongquangit@gmail.com>
2024-09-30 23:04:42 +0700

    Quang | Done navbar

---
+++ frontend/src/pages/home/HomePage.jsx
@@ -0,0 +1,10 @@
+import NavBar from './NavBar.jsx'
+
+const HomePage = () => {
+  return (
+    <div>
+      <NavBar />
+    </div>
+  );
+}
+export default  HomePage
---
+++ frontend/src/pages/home/NavBar.jsx
@@ -0,0 +1,36 @@
+import logo from '../../assets/logo.png';
+import {Link} from "react-router-dom";
+
+const NavBar = () => {
+    return (
+        <div className='h-screen w-screen'>
+            <div className='w-full h-[7%] flex flex-row flex-wrap'>
+                <div className='h-full w-1/4'>
+                    <Link to={'/'} className='flex flex-row h-full w- ml-3'>
+                        <img src={logo} alt='logo' className='h-full ml-1'/>
+                        <h1 className='ml-2 flex items-center font-abeezee font-bold text-3xl'>Narcissus</h1>
+                    </Link>
+                </div>
+                <div className='w-2/4 flex justify-center items-center'>
+                    <div className='bg-[rgba(58,54,253,0.4)] flex flex-row w-full rounded-md shadow-sm'>
+                        <input
+                            type='text'
+                            className='bg-transparent w-5/6 ml-3 focus:outline-none text-white text-sm '
+                        />
+                        <button className='bg-transparent w-1/6 rounded-r-md text-white font-abeezee'>Search</button>
+                    </div>
+                </div>
+                <div className='w-1/4 flex flex-row justify-center items-center'>
+                    <div>
+                        <Link className='mr-4 ml-4 text-[#FF0099] font-abeezee font-bold' to={'/login'}>Account</Link>
+                    </div>
+                    <div>
+                        <Link className='ml-4 mr-4 text-[#FF0099] font-abeezee font-bold' to={'/contact'}>Contact</Link>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default NavBar;
