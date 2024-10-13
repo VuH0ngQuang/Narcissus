69a94de444045ff6883bc7e6bd9cf150fb1c74c0
unknown <hatantai.it@gmail.com>
2024-10-13 20:50:26 +0700

    Tai | Base of 8 product details page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,6 +3,7 @@ import HomePage from './pages/home/HomePage.jsx';
 import Login from './pages/account/Login.jsx';
 import Register from "./pages/account/Register.jsx";
 import AdminDashboard from "./pages/admin/AdminDashboard.jsx";
+import ProductDetailPage from './pages/home/products/ProductDetailPage.jsx';
 import Order from "./pages/admin/Order.jsx"
 
 const App = () => {
@@ -13,6 +14,7 @@ const App = () => {
                 <Route path="/login" element={<Login />} />
                 <Route path="/register" element={<Register />} />
                 <Route path="/admin/dashboard" element={<AdminDashboard />} />
+                <Route path="/product/:id" element={<ProductDetailPage />} /> {/* Add this line */}
                 <Route path="/admin/addproduct" element={<h1>Not Found</h1>} />
                 <Route path="/admin/updateproduct" element={<h1>Not Found</h1>} />
                 <Route path="/admin/order" element={<Order />} />
