bd16befa5f4d533d52ae47b230c33868bc47391f
VuH0ngQuang <vuhongquangit@gmail.com>
2024-12-15 22:53:34 +0700

    Quang |

--- backend/src/main/java/com/narcissus/backend/security/SecurityConfig.java
+++ backend/src/main/java/com/narcissus/backend/security/SecurityConfig.java
@@ -47,6 +47,7 @@ public class SecurityConfig {
                 )
                 .authorizeHttpRequests((authorizeHttpRequests) -> authorizeHttpRequests
                         .requestMatchers(HttpMethod.GET, "/api/products/**").permitAll()
+                        .requestMatchers(HttpMethod.POST, "/api/products/review/add/").authenticated()
                         .requestMatchers(HttpMethod.POST, "/api/products/**").hasRole("ADMIN")
                         .requestMatchers(HttpMethod.DELETE, "/api/products/**").hasRole("ADMIN")
                         .requestMatchers("/api/auth/**").permitAll()
