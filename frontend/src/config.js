6c6bed68d2afdf53d41d9e980d3fbfc65685b773
VuH0ngQuang <vuhongquangit@gmail.com>
2025-01-04 12:06:50 +0700

    Quang | Change URL of frontend

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,4 +1,4 @@
 export const host = "http://20.89.177.142:8080/api";
-export const FEHost = "http://narcissusflowers.japaneast.cloudapp.azure.com:5173/";
+export const FEHost = "http://narcissusflowers.japaneast.cloudapp.azure.com:5173";
 // export const host = "http://localhost:8080/api";
 // export const FEHost = "http://localhost:5173";
